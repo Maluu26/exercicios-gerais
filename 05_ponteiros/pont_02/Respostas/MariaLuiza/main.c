241afd229eb780799fbb3d3f629fd4582beb6af2
Maluu26 <142928665+Maluu26@users.noreply.github.com>
2023-09-28 12:00:25 -0300

    Create main.c

---
+++ 05_ponteiros/pont_02/Respostas/MariaLuiza/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main()
+{
+    int casos;
+
+    scanf("%d", &casos);
+    while (casos)
+    {
+        int m, n, menor, maior;
+        LeIntervalo(&m, &n);
+
+        ObtemMaiorEMenorPrimo(m, n, &menor, &maior);
+
+        printf("%d %d\n", menor, maior);
+
+        casos--;
+    }
+    return 0;
+}
