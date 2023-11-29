5cb5f9f2c28e8277ff1d100b2009a9286a88d192
Malu26 <maria.lfr.santos@edu.ufes.br>
2023-11-29 14:45:13 -0300

    1

---
+++ 08_TAD_generico/TAD_gen_01/Respostas/tadgen.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"tadgen.h"
+ struct generic {
+    void *vetor;
+};
+/**
+ * @brief Cria uma estrutura genérica e dinâmica que pode armazenar tanto inteiros quanto floats
+ *
+ * @param type - Tipo do vetor genérico. Segue o Enum definido acima.
+ * @param numElem A quantidade de elementos que serão armazenados no vetor
+ *
+ * @return O vetor genérico
+ */
+tGeneric* CriaGenerico(Type type, int numElem){
+    tGenerico *gen = malloc(sizeof(tGenerico));
+    if(type == INT){
+        gen->vetor = (int*)malloc(numElem*sizeof(int));
+    }else{
+         gen->vetor = (float*)malloc(numElem*sizeof(float));
+    }
+    return gen;
+}
+
+/**
+ * @brief Destroi o vetor genérico (libera a memória alocada dinamicamente)
+ *
+ * @param gen - O vetor genérico que terá seu conteúdo liberado/destruído
+ */
+void DestroiGenerico(tGeneric* gen);
+
+/**
+ * @brief Lê o conteúdo do vetor genérico e salva no local adequado
+ *
+ * @param gen - O vetor genérico que terá seu conteúdo preenchido/lido
+ */
+void LeGenerico(tGeneric* gen);
+
+/**
+ * @brief Busca uma conta na agencia que possui um id específico
+ *
+ * @param gen - O vetor genérico que terá seu conteúdo impresso em tela
+ */
+void ImprimeGenerico(tGeneric* gen);
